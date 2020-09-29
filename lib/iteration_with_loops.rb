8ffd05803d04e8c1166a5fdb68208d7beb95cc0d
Anne Nardolilli <anardolilli@gmail.com>
2020-09-29 00:30:35 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,23 +4,20 @@ mixed_data = [
   ["studied", 101, 233, "Ruby"]
 ]
 
-#def join_nested_strings(src)
-# total = ""
-# row_index = 0 
-# new_string_array = []
-#   while row_index < src.count do 
-#     element_index = 0 
-#     while element_index < src[row_index].count do 
-#       if src[row_index][element_index].class == String 
-#         total += src[row_index][element_index]
-#         element_index += 1
-#         row_index += 1 
-#       end 
-    
-#   end 
-# end 
-
-# new_string_array
- 
-# end
+def join_nested_strings(src)
+total = ""
+row_index = 0 
+#new_string_array = []
+  while row_index < src.count do 
+    element_index = 0 
+    while element_index < src[row_index].count do 
+      if src[row_index][element_index].class == String 
+        total += src[row_index][element_index]
+      end
+        element_index += 1
+    end 
+        row_index += 1 
+  end 
+  total 
+end
 
